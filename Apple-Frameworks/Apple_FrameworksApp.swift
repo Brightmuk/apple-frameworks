aede2b1a9e1ef1738eaaf25ab0f24184ce4cc090
Brimuk <be.rightmuk@gmail.com>
2025-07-15 16:54:12 +0300

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Bright Mukonesi on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
